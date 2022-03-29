90a76aec85444b8e605fc28c1fc2edcc133da2a8
Felipe Caires <caireskode@gmail.com>
2022-03-28 21:52:01 -0300

    feat(register-page):atualização de branch

--- App/src/screens/SignUp/index.js
+++ App/src/screens/SignUp/index.js
@@ -38,7 +38,7 @@ export default ()=>{
 
     return(
         <Container>
-            <BarberLogo width="100%" height="160"/>
+            <BarberLogo width="100%" height="160" />
 
             <InputArea>
             <SignInput
